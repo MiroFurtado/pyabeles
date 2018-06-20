e40087415de8274416a272f3a679cf49727480c5
Miro Furtado <furtadomiro@gmail.com>
2018-06-20 15:05:02 -0400

    Adding license, setting up for PyPi, and updating gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pyabeles',
     version='0.1dev',
-    packages=['towelstuff',],
+    author='Miro Furtado',
+    author_email="furtadomiro@gmail.com",
     license='Apache License 2.0',
 )
