90f95b26eb691524c710c0d111cd8227ae3b952c
Miro Furtado <furtadomiro@gmail.com>
2018-07-02 16:39:56 -0400

    more pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyabeles',
-    version='0.1dev',
+    version='0.2.0',
     author='Miro Furtado',
     author_email="furtadomiro@gmail.com",
     license='Apache License 2.0',
